e31467a4dfacd5344004c6427733dec2c3ffa6f0
Bilal Tariq <billal.tariq03@gmail.com>
2025-04-25 22:26:20 +0500

    set backend for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
+
 const productRoutes = require("./routes/productRoutes");
 const DealRoutes = require("./routes/DealRoutes");
 const SubCategoryRoutes = require("./routes/SubCategoryRoutes");
@@ -13,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/ecommerce", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
