69ebdc35b81181c12732cae326f7e5683f91bc1f
Bilal Tariq <billal.tariq03@gmail.com>
2025-04-29 02:22:01 +0500

    made some changes to deploy

--- server/server.js
+++ server/server.js
@@ -18,12 +18,15 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend server!");
+});
 
 
 //Routes
